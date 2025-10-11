50f45a130b66491b5b871706c1de61de9bc3ff31
kuryx <andersonsanchez321@gmail.com>
2025-10-11 15:15:58 -0500

    Revert "Mejoré el dashboard y agregué react-icons"
    This reverts commit fad5e9913bf506f2812fcdec6a4a05e4af9cab05.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import './index.css';
-import "./assets/css/argon-dashboard-react.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
